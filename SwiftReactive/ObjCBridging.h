e62b801705bdcf02c0c3ba571f45f6e01cb14f82
Luke Schoen <ltfschoen@gmail.com>
2015-12-16 02:36:19 -0300

    Added Objective-C Bridging header file

---
+++ SwiftReactive/ObjCBridging.h
@@ -0,0 +1,18 @@
+//
+//  ObjCBridging.h
+//  SwiftReactive
+//
+//  Created by LS on 16/12/2015.
+//  Copyright © 2015 Luke Schoen. All rights reserved.
+//
+
+#ifndef ObjCBridging_h
+#define ObjCBridging_h
+
+/* Import so Swift recognises referenced methods and Objective-C classes
+ */
+#import <ReactiveFoundation/ReactiveFoundation.h>
+#import <ReactiveKit/ReactiveKit.h>
+#import <ReactiveUIKit/ReactiveUIKit.h>
+
+#endif /* ObjCBridging_h */
